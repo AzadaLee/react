e608f07103ae70b27d9ac56de7cd3508757ada32
AzadaLee <txm0110@sina.com>
2016-06-23 15:56:07 +0800

    fff

---
+++ demo/component/loginComponent.js
@@ -0,0 +1,7 @@
+var react = require('../../build/react');
+var react = require('../../build/react-dom');
+var react = require('../../build/browser.min');
+module.exports = Login;
+function Login(){
+
+}
